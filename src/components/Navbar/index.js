1511c3b32bac0e642bd819f6df332bf19c5f7572
Aurèle Oulès <aurele@oules.com>
2019-01-24 14:36:32 +0100

    navigation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,7 @@
 import React from 'preact';
 
+import {Link} from 'preact-router';
+
 import './styles.scss';
 
 import strings from 'strings';
@@ -10,6 +12,23 @@ class Navbar extends React.Component {
             <div className="navbar-container">
                 <div className="navbar">
                     <h1 className="logo">Backpulse</h1>
+                    <ul>
+                        <li>
+                            <a rel="noopener noreferrer" target="_blank" href="https://docs.backpulse.io">
+                                {strings.MENU_DOCUMENTATION}
+                            </a>
+                        </li>
+                        <li>
+                            <Link href="/pricing">
+                                {strings.MENU_PRICING}
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/help">
+                                {strings.MENU_HELP}
+                            </Link>
+                        </li>
+                    </ul>
                     <div className="right">
                         <a className="white button" href="https://dashboard.backpulse.io/login">{strings.LOGIN}</a>
                         <a className="primary button" href="https://dashboard.backpulse.io/signup">{strings.SIGNUP}</a>
--- src/strings.js
+++ src/strings.js
@@ -9,7 +9,11 @@ const strings = {
 
         "HOME_TITLE": "Laissez nous faire le reste",
         "HOME_DESCRIPTION": "Backpulse vous permet de créer des sites web, sans avoir à se soucier de la partie administration.",
-        "GET_STARTED": "S'inscrire !"
+        "GET_STARTED": "S'inscrire !",
+        "MENU_DOCUMENTATION": "Documentation",
+        "MENU_PRICING": "Prix",
+        "MENU_HELP": "Aide"
+
     },
     en: {
         "LOGIN": "Log in",
@@ -17,7 +21,11 @@ const strings = {
         
         "HOME_TITLE": "Let us do the hard work",
         "HOME_DESCRIPTION": "Backpulse lets you build you own website, without having to care about the content administration.",
-        "GET_STARTED": "Get started!"
+        "GET_STARTED": "Get started!",
+
+        "MENU_DOCUMENTATION": "Documentation",
+        "MENU_PRICING": "Pricing",
+        "MENU_HELP": "Help"
     }
 };
 
